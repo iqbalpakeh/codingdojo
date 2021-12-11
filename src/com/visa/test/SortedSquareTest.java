1f3d7e8e0d8a3239a84eb80bc79266774ee08d3e
Progrema Studio <iqbalpakeh@gmail.com>
2021-12-11 10:11:38 +0800

    Add more test

--- src/com/visa/test/SortedSquareTest.java
+++ src/com/visa/test/SortedSquareTest.java
@@ -7,6 +7,11 @@ public class SortedSquareTest {
 
     public static void test() {
         {
+            int[] data = new int[]{-4,-1,0,3,10};
+            int[] exp = new int[]{0,1,9,16,100};
+            Util.compare(SortedSquare.sortedSquares(data), exp);
+        }
+        {
             int[] data = new int[]{-7, -3, 2, 3, 11};
             int[] exp = new int[]{4, 9, 9, 49, 121};
             Util.compare(SortedSquare.sortedSquares(data), exp);
