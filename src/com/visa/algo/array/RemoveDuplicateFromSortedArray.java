9d7c74008831f50bbb3c36af08bc26192a3e8a47
Progrema Studio <iqbalpakeh@gmail.com>
2021-12-13 06:23:55 +0800

    add comment

--- src/com/visa/algo/array/RemoveDuplicateFromSortedArray.java
+++ src/com/visa/algo/array/RemoveDuplicateFromSortedArray.java
@@ -13,12 +13,32 @@ package com.visa.algo.array;
  *
  * Do not allocate extra space for another array. You must do this by modifying the
  * input array in-place with O(1) extra memory.
+ *
+ * Example 1:
+ * Input: nums = [1,1,2]
+ * Output: 2, nums = [1,2,_]
+ * Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+ * It does not matter what you leave beyond the returned k (hence they are underscores).
+ *
+ * Example 2:
+ * Input: nums = [0,0,1,1,1,2,2,3,3,4]
+ * Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+ * Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+ * It does not matter what you leave beyond the returned k (hence they are underscores).
+ *
  */
 
 public class RemoveDuplicateFromSortedArray {
 
     public static int removeDuplicates(int[] nums) {
         return 0;
+    }
+
+    private static int removeElementByOffset(int[] nums, int offset, int size) {
+        for (int i = offset; i < size - 1; i++) {
+            nums[i] = nums[i + 1];
+        }
+        return --size;
     }
 
 }
