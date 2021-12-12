0b387f4da02a85c4d83f0d15b6b2fb219f984131
Progrema Studio <iqbalpakeh@gmail.com>
2021-12-12 17:51:30 +0800

    Add remove element

--- src/com/visa/algo/array/RemoveElement.java
+++ src/com/visa/algo/array/RemoveElement.java
@@ -38,7 +38,7 @@ public class RemoveElement {
             }
         }
         int k = nums.length;
-        for (int i = 0; i < nums.length - 1; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if (i == k) {
                 return k;
             }
