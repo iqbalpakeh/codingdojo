43bf786c7c46fc7f02ea0d16486bb6b6e9433a57
Progrema Studio <iqbalpakeh@gmail.com>
2021-12-15 22:16:47 +0800

    fix the test code

--- src/com/visa/test/array/RemoveDuplicateFromSortedArrayTest.java
+++ src/com/visa/test/array/RemoveDuplicateFromSortedArrayTest.java
@@ -28,7 +28,7 @@ public class RemoveDuplicateFromSortedArrayTest {
             int[] input = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
             int[] exp = new int[]{0, 1, 2, 3, 4};
             int k = RemoveDuplicateFromSortedArray.removeDuplicates(input);
-            Util.compare(k, 2);
+            Util.compare(k, 5);
             boolean isError = false;
             for (int i = 0; i < k; i++) {
                 if (input[i] != exp[i]) {
